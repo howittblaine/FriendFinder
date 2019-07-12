3b88299b5fa572132d0fae686eb29047cba18657
Blaine Howitt <howittblaine@gmail.com>
2019-07-11 23:50:27 -0700

    Added Friend List

--- JavaScript/Routing/apiRoutes.js
+++ JavaScript/Routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friendsList = require("../JavaScript/friends");
+var friendsList = require("../JavaScript/friends.js");
 
 module.exports = function(app){
 	app.get("api/friends", function(req, res){
--- JavaScript/friends.js
+++ JavaScript/friends.js
@@ -1,20 +1,20 @@
 var friendsList = [
 	{
-	  name: 
-	  photo: 
-	  preferences:
+	  name: "Damian",
+	  photo: "https://www.stickpng.com/assets/images/584566571bb5ce41bdb89076.png",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
 	},
 
 	{
-	  name:
-	  photo: 
-	  preferences:
+	  name: "Jusuf",
+	  photo: "https://fansrule.files.wordpress.com/2019/01/Jusuf-Nurkic.png?w=1200&h=627&crop=1",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
 	},
 
 	{
-	  name:
-	  photo: 
-	  preferences: 
+	  name: "Chance",
+	  photo: "https://s3.amazonaws.com/radiomilwaukee-wordpress-uploads/wp-content/uploads/2018/08/20093852/main-qimg-1170ee193cdd6502aae0c58d0559962c-c.jpg",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
 	},
 ];
 
