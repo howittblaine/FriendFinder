f4264e1466b6f5cb300bf023bb14e891becce9db
Blaine Howitt <howittblaine@gmail.com>
2019-07-11 23:54:21 -0700

    Changed home.html to index.html

--- JavaScript/Routing/htmlRoutes.js
+++ JavaScript/Routing/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = function(app){
 // HTML : Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/HTML/home.html"));
+  res.sendFile(path.join(__dirname, "/HTML/index.html"));
 });
 
 app.get("/survey", function(req, res) {
